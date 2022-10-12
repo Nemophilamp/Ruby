4445a101c2efff8a0909382b5cff217c8846957f
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-12 05:46:38 +0000

    [Finish]Ruby-8

--- ８章/fizz_buzz.rb
+++ ８章/fizz_buzz.rb
@@ -1,10 +1,22 @@
+# 3と5の両方で割り切れる数値 15　なら FizzBuzz
+# ３で割り切れるならFizz
+# ５で割り切れるならBuzz
+
 def fizz_buzz(number)
-  # ここにFizzBuzzプログラムの処理を書いてください
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else
+    number.to_s    # 文字列に変えて返す
+  end
 end
 
-puts "数字を入力してください。"
+puts "数字を入力してください"
 
 input = gets.to_i
 
-puts "結果は..."
+puts "結果は…"
 puts fizz_buzz(input)
