b1446dee6bf388ab276b42fdde6db0fb1d0cc056
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-12 02:22:05 +0000

    [Finish]Ruby-6

--- ６章/apple.rb
+++ ６章/apple.rb
@@ -1,4 +1,4 @@
-apple = "Nagano"
+apple = "Yamanashi"
 
 if apple == "Aomori"
   puts "このリンゴは青森県産です。"
@@ -6,4 +6,4 @@ elsif apple == "Nagano"
   puts "このリンゴは青森県産ではなく、長野県産です。"
 else
   puts "このリンゴは青森県産でも長野県産でもありません。"
- end
+end
