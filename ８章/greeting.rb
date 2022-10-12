875d46a3bed8a5a2e21dd0d0ebc648ea7feebca5
EC2 Default User <ec2-user@ip-172-31-0-63.ec2.internal>
2022-10-12 03:44:39 +0000

    [Add]greeting 下に書いた方が優先される

--- ８章/greeting.rb
+++ ８章/greeting.rb
@@ -1,5 +1,6 @@
-def greeting
-  "Hello"
+def greeting(name)
+  "Hello, #{name}!" # この行のnameは、引数で渡すname ☆彡戻り値
+  "Good morning, #{name}!"
 end
 
-puts greeting
+puts greeting("John")  # 'John'を引数として渡す
